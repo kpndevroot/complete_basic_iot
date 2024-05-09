1889b833f29d736980789644b3d39daf110aa9a0
kpndevroot <vishnukpn884@gmail.com>
2024-05-09 20:06:19 +0530

    time stamp added

--- api/modal/ipModal.js
+++ api/modal/ipModal.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
 // create modal schema
-const ipSchema = new mongoose.Schema({
-  ip: {
-    type: String,
-    required: true,
+const ipSchema = new mongoose.Schema(
+  {
+    ip: {
+      type: String,
+      required: true,
+    },
+    lastUpdated: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  {timestamps: true},
+);
 // module.exports = mongoose.model('ip', ipSchema);
 const ipModal = mongoose.model('Ip', ipSchema);
 
