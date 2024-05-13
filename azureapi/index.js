513954219b5c961747e5696c320107ee3f92d66d
kpndevroot <vishnukpn884@gmail.com>
2024-05-14 00:41:05 +0530

    fix

--- azureapi/index.js
+++ azureapi/index.js
@@ -22,6 +22,7 @@ const Tunnel = mongoose.model('Tunnel', TunnelSchema);
 app.get('/tunnel', async (req, res) => {
   try {
     let doc = await Tunnel.findOne({});
+    console.log({doc: doc});
     res.status(200).json({doc: doc, status: true});
   } catch (error) {
     res.status(500).json({error: error.message});
